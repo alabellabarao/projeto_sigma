615d6e8671fc7817d48f4fd1025a8b234bbd52a3
Ana Barão <alabella@gmail.com>
2024-05-26 15:01:08 -0300

    Alteração no código

--- cypress/e2e/esqueceuSenha.cy.js
+++ cypress/e2e/esqueceuSenha.cy.js
@@ -1,23 +1,27 @@
+
+/// <reference types="cypress" />
+
 const CPF = '320.100.318-22'
 describe ('Senha', () => {
     
     it ('Esqueceu a senha', () => {
         cy.visit('/')
             .get('.title')
+            .should('be.visible')
             .contains('Login')
+            .should('be.visible')
         cy.get('.options .v-button')
+            .should('be.visible')
             .click()
         cy.get('.radio-group input[type="radio"][name="cpf"]')
             .check({force: true})
             .should('be.checked')
         cy.get('input[placeholder="Digite o CPF cadastrado no SIGMA"]')
-            .should('be.visible')
-           .type(CPF)
+            .type(CPF)
         cy.get('[data-test="submit-btn"]')
-           .click();
+            .should('be.visible')
+            .click();
         cy.get('.message')
            .should('contain', 'Enviamos instruções de redefinição de senha para o e-mail cadastrado.');
-
     })
-
 })
